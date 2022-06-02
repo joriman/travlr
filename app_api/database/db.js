1f34ce08bcc43b2ebb79eaaf5fcf5809d26f1cc9
JJ0rd4n <jamonj@protonmail.com>
2022-06-02 10:43:07 +0200

    Create new application API

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,28 +1,41 @@
 const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = `mongodb://${host}/Documents/dev/web/travlr`;
+const host = process.env.DB_HOST || '127.0.0.1';
+const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
 
-//avoid 'current Server Discovery and Monitoring engine is deprecated'
+//avoid current server discovery and monitoring engine is deprecated
 mongoose.set('useUnifiedTopology', true);
 
 const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }), 1000);
-}
+    setTimeout(
+      () =>
+        mongoose.connect(dbURI, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+        }), 1000);
+};
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
     console.log(`Mongoose connected to ${dbURI}`);
 });
-mongoose.connection.on('error', err => {
-    console.log('Mongoose connection error:', err);
-    return connect();
+  
+mongoose.connection.on("error", (err) => {
+    console.log(`Mongoose connection error: `, err);
 });
-mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
+  
+mongoose.connection.on("disconnected", () => {
+    console.log(`Mongoose disconnected`);
 });
+
+if (process.platform == "win32") {
+    const rl = readLine.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.on("SIGINT", () => {
+      process.emit("SIGINT");
+    });
+  }
 
 const gracefulShutdown = (msg, callback) => {
     mongoose.connection.close( () => {
@@ -51,4 +64,7 @@ process.on('SIGTERM', () => {
     });
 });
 
-require('./models/travlr');
+connect();
+
+// bring in schema
+require("./models/travlr");
