091188a5f95eba58060d85de84f80717a5c5f054
JJ0rd4n <jamonj@protonmail.com>
2022-06-09 22:53:55 +0200

    refactor trip render logic into Angular component (trip-card)

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-trip-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trip-card.component.css']
 })
 export class TripCardComponent implements OnInit {
+
+  @Input('trip') trip: any;
 
   constructor() { }
 
